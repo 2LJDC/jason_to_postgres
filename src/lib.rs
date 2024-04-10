a8076c09b13f17ccf39f4f3ea24f009cda4d5284
cs97 <chrswr97@gmail.com>
2024-04-10 15:22:44 +0200

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -23,7 +23,7 @@ pub async fn dump_database(url: &str) -> Result<String, Box<dyn stdError>> {
             bottom: s.7,
             status: s.8,
         };
-        
+        arr.remove(arr.len() - 1);
         arr = format!("{arr}{},", single_row);
     }
     
